eaae65e8ede6d20ec21d5a0de18c162a2d18f66c
Andrey <andrey.gtx@gmail.com>
2024-07-05 15:39:30 +0300

    Verilog implenentation of 1024-bit shift register.

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_i1404 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,8 +16,24 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  parameter DEPTH = 1024;
+
+  reg [DEPTH-1:0] shift_reg;
+  wire din, clken, dout;
+  assign din = uio_in[0];
+  assign clken = ui_in[0];
+  assign dout = shift_reg[DEPTH-1];
+
+  always @(posedge clk) begin
+    if (clken) begin
+      shift_reg[DEPTH-1:1] <= shift_reg[DEPTH-2:0];
+      shift_reg[0] <= din;
+    end
+  end;
+
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out  = dout;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
